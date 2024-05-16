bf2f5a7fa6572fd8295d655a8d64e17a56b77044
Uzurch1k <galcenkovlad7@gmail.com>
2024-05-16 20:43:01 +0300

    fix

--- src/js/parts/forms.js
+++ src/js/parts/forms.js
@@ -1,15 +1,51 @@
-function checkSurveyArea() {
-  const inputs = document.querySelectorAll('.survey-area input[type="radio"]');
-  const buttonNext = document.querySelector('.form-btn-next');
+const refs = {
+  buttonNext: document.querySelector('.form-btn-next'),
+  formCheck: document.querySelectorAll('.form-choose'),
 
-  buttonNext.disabled = true;
-  buttonNext.classList.add('disabled');
+  surveyArea: document.querySelector('.survey-area'),
+  allergy: document.querySelector('.allergy'),
+};
 
-  inputs.forEach(input => {
-    input.addEventListener('change', () => {
-      buttonNext.disabled = true;
-      buttonNext.classList.remove('disabled');
-    });
-  });
-}
-checkSurveyArea();
+// refs.buttonNext.addEventListener('click', () => {
+//   for (let i = 0; i < refs.formCheck.length; i++) {
+//     const formCheck = refs.formCheck[i];
+//     if (formCheck.classList.contains('active')) {
+//       const nextFormCheck = refs.formCheck[i + 1];
+//       if (nextFormCheck) {
+//         formCheck.classList.remove('active');
+//         nextFormCheck.classList.add('active');
+//         return; // Вийти з циклу після зміни активного елемента
+//       }
+//     }
+//   }
+// });
+
+// function checkSurveyArea() {
+//   const inputs = document.querySelectorAll('.survey-area input[type="radio"]');
+
+//   refs.buttonNext.disabled = true;
+//   refs.buttonNext.classList.add('disabled');
+
+//   inputs.forEach(input => {
+//     input.addEventListener('change', () => {
+//       refs.buttonNext.disabled = false;
+//       refs.buttonNext.classList.remove('disabled');
+//     });
+//   });
+// }
+// checkSurveyArea();
+
+// function checkAllergy() {
+//   const inputs = document.querySelectorAll('.allergy input[type="radio"]');
+
+//   refs.buttonNext.disabled = true;
+//   refs.buttonNext.classList.add('disabled');
+
+//   inputs.forEach(input => {
+//     input.addEventListener('change', () => {
+//       refs.buttonNext.disabled = false;
+//       refs.buttonNext.classList.remove('disabled');
+//     });
+//   });
+// }
+// checkAllergy();
