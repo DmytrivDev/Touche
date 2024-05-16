defbc28b0d8b27e79a153594089e7554e7d03d49
Uzurch1k <galcenkovlad7@gmail.com>
2024-05-16 23:32:47 +0300

    fix

--- src/js/parts/forms.js
+++ src/js/parts/forms.js
@@ -1,54 +1,52 @@
-const refs = {
-  buttonNext: document.querySelectorAll('.form-btn-next'),
-  formActive: document.querySelector('.form-active'),
-
-  surveyArea: document.querySelector('.survey-area'),
-  allergy: document.querySelector('.allergy'),
-};
+const buttonNext = document.querySelectorAll('.form-btn-next');
 
 function nextStep() {
-  if (refs.buttonNext) {
-    refs.buttonNext.forEach(el => {
-      el.addEventListener('click', evt => {
-        evt.preventDefault();
+  buttonNext.forEach(elem => {
+    elem.addEventListener('click', e => {
+      e.preventDefault();
 
-        const button = evt.currentTarget;
-        const dataId = button.dataset.id;
-        const nextStep = document.getElementById(dataId);
+      const button = e.currentTarget;
+      const dataId = button.dataset.id;
+      const nextStep = document.getElementById(dataId);
 
-        refs.formActive.classList.remove('form-active');
+      if (!button.classList.contains('disabled')) {
+        const formActive = document.querySelector('.form-active');
+        formActive.classList.remove('form-active');
         nextStep.classList.add('form-active');
-      });
+      }
     });
-  }
+  });
 }
 nextStep();
-// function checkSurveyArea() {
-//   const inputs = document.querySelectorAll('.survey-area input[type="radio"]');
 
-//   refs.buttonNext.disabled = true;
-//   refs.buttonNext.classList.add('disabled');
+function checkSurveyArea() {
+  const inputs = document.querySelectorAll('.survey-area input[type="radio"]');
+  const btnNext = document.querySelector('.survey-area__btn');
 
-//   inputs.forEach(input => {
-//     input.addEventListener('change', () => {
-//       refs.buttonNext.disabled = false;
-//       refs.buttonNext.classList.remove('disabled');
-//     });
-//   });
-// }
-// checkSurveyArea();
+  btnNext.disabled = true;
+  btnNext.classList.add('disabled');
 
-// function checkAllergy() {
-//   const inputs = document.querySelectorAll('.allergy input[type="radio"]');
+  inputs.forEach(input => {
+    input.addEventListener('change', () => {
+      btnNext.disabled = false;
+      btnNext.classList.remove('disabled');
+    });
+  });
+}
+checkSurveyArea();
 
-//   refs.buttonNext.disabled = true;
-//   refs.buttonNext.classList.add('disabled');
+function checkAllergy() {
+  const inputs = document.querySelectorAll('.allergy input[type="radio"]');
+  const btnNext = document.querySelector('.allergy__btn');
 
-//   inputs.forEach(input => {
-//     input.addEventListener('change', () => {
-//       refs.buttonNext.disabled = false;
-//       refs.buttonNext.classList.remove('disabled');
-//     });
-//   });
-// }
-// checkAllergy();
+  btnNext.disabled = true;
+  btnNext.classList.add('disabled');
+
+  inputs.forEach(input => {
+    input.addEventListener('change', () => {
+      btnNext.disabled = false;
+      btnNext.classList.remove('disabled');
+    });
+  });
+}
+checkAllergy();
