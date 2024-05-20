7e4c1eb00210547938e34fd464afbbb2871b1ebd
Uzurch1k <galcenkovlad7@gmail.com>
2024-05-20 19:59:00 +0300

    fix

---
+++ src/js/parts/forms/reservation.js
@@ -0,0 +1,23 @@
+import flatpickr from 'flatpickr';
+import { French } from 'flatpickr/dist/l10n/fr.js';
+
+const reservationDate = document.querySelector('.flatpickr input');
+
+flatpickr(reservationDate, {
+  locale: French,
+  altInput: true,
+  altFormat: 'M j, Y',
+  dateFormat: 'd.m.Y',
+  showMonths: 1,
+  minDate: 'today',
+  shorthandCurrentMonth: true,
+  maxDate: new Date().fp_incr(21),
+  inline: true,
+});
+const style = document.createElement('style');
+style.innerHTML = `
+    .flatpickr-calendar .dayContainer span:nth-child(n+36) {
+      display: none !important;
+    }
+  `;
+document.head.appendChild(style);
