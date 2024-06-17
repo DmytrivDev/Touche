cc934ad9cad5b27ed9ff309253e773ff50e66c01
Uzurch1k <galcenkovlad7@gmail.com>
2024-06-17 22:47:46 +0300

    fix

--- src/js/parts/forms/reservation.js
+++ src/js/parts/forms/reservation.js
@@ -1,6 +1,8 @@
 import flatpickr from 'flatpickr';
 import { French } from 'flatpickr/dist/l10n/fr.js';
 
+import 'flatpickr/dist/flatpickr.min.css';
+
 flatpickr('#reservation-date', {
   locale: French,
   altInput: true,
